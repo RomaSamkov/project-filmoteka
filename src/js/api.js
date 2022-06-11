aba8a361d7aa9ff381d3195f1b148a0a14fa34d1
Maksym Padalka <sheihmaks@gmail.com>
2022-06-11 15:43:25 +0300

    Onfetch1

--- src/js/api.js
+++ src/js/api.js
@@ -4,7 +4,7 @@ const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${AP
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}`;
 
-export class filmsForUser {
+export default class ApiServise {
   constructor() {
     this.userSearch = '';
     this.page = 1;
