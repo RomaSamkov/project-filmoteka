c8b1dfa879d769fe67560f3cc5eda02c51de8b21
RomaSamkov <Romansamkov.rs@gmail.com>
2022-06-12 22:29:36 +0300

    add export API_KEY

--- src/js/api.js
+++ src/js/api.js
@@ -1,10 +1,10 @@
 const axios = require('axios').default;
 
-const API_KEY = 'f534638cb3304b9759e126ecf8f1bc28';
-const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`;
+export const API_KEY = 'f534638cb3304b9759e126ecf8f1bc28';
+export const BASE_URL = 'https://api.themoviedb.org/3';
+export const API_URL = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`;
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
-const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}`;
+export const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}`;
 
 export default class ApiServise {
   constructor() {
