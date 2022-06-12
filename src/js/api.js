9f4e38c3f7b4084577be30df61668f5e0e2b7fd0
RomaSamkov <Romansamkov.rs@gmail.com>
2022-06-12 10:54:05 +0300

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,6 +1,7 @@
 const API_KEY = 'f534638cb3304b9759e126ecf8f1bc28';
 const BASE_URL = 'https://api.themoviedb.org/3';
-const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+// const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
+const API_URL = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`;
 const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}`;
 
