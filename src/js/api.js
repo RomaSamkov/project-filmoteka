44aa13de34c193f08f8fb2bc60c7c7352bdd7c31
RomaSamkov <Romansamkov.rs@gmail.com>
2022-06-12 19:14:07 +0300

    Update api.js

--- src/js/api.js
+++ src/js/api.js
@@ -1,11 +1,7 @@
 const axios = require('axios').default;
 
 const API_KEY = 'f534638cb3304b9759e126ecf8f1bc28';
-
-export const API_KEY = 'f534638cb3304b9759e126ecf8f1bc28';
-
 const BASE_URL = 'https://api.themoviedb.org/3';
-// const API_URL = `${BASE_URL}/discover/movie?sort_by=popularity.desc&api_key=${API_KEY}`;
 const API_URL = `${BASE_URL}/trending/movie/day?api_key=${API_KEY}`;
 export const IMG_URL = 'https://image.tmdb.org/t/p/w500';
 const SEARCH_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}`;
@@ -35,7 +31,7 @@ export default class ApiServise {
   incrementPage() {
     this.page += 1;
   }
-  decrementPage(){
+  decrementPage() {
     this.page -= 1;
   }
   setPage(numberPage) {
