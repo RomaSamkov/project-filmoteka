6282d86d1f6868b18389c26dee78522aa86b1a5c
RomaSamkov <Romansamkov.rs@gmail.com>
2022-06-13 14:44:34 +0300

    Update refs.js

--- src/js/refs.js
+++ src/js/refs.js
@@ -2,10 +2,10 @@ export const refs = {
   formSearch: document.querySelector('.header__form'),
   galleryContainer: document.querySelector('.gallery'),
   filmsContainer: document.querySelector('.film__list'),
-  closeModalBtn: document.querySelector(".modal-closeButton"),
+  closeModalBtn: document.querySelector('.modal-closeButton'),
   backdrop: document.querySelector('.modal-backdrop'),
   modalContainer: document.querySelector('.modal-container'),
   modalBtnEl: document.querySelector('.modal-button'),
-  modalListEl: document.querySelector('.modal-button-list')
+  modalListEl: document.querySelector('.modal-button-list'),
   pagination: document.querySelector('.pagination'),
 };
