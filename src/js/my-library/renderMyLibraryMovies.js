d48371ac8204a856fc2ba6c3bf94e9255c10dc39
RomaSamkov <Romansamkov.rs@gmail.com>
2022-06-19 23:15:19 +0300

    Update renderMyLibraryMovies.js

--- src/js/my-library/renderMyLibraryMovies.js
+++ src/js/my-library/renderMyLibraryMovies.js
@@ -1,34 +1,31 @@
-import {refs} from "../refs";
-import movieTemplate from "../moivieTamplate";
+import { refs } from '../refs';
+import movieTemplate from '../movieTamplate';
 
-function renderWatchedMovies(){
-    const films = JSON.parse(localStorage.getItem('watched'));
-    
-    if (films.length === 0){
-        // const screen = 'src="./images/empty.jpeg"'
-        // const markup = `<img ${screen} alt="empty list"><p>Your collection list is empty.</p>`;
-        
-        const div = `<div class="film__list-empty-container"><p>Your collection list is empty.</p></div>`
-        
-        refs.filmsContainer.innerHTML = div;
-    }else {
+function renderWatchedMovies() {
+  const films = JSON.parse(localStorage.getItem('watched'));
 
-        const markup = films.map(movieTemplate);
-        refs.filmsContainer.innerHTML = markup.join('');
-    }
-};
+  if (films.length === 0) {
+    // const screen = 'src="./images/empty.jpeg"'
+    // const markup = `<img ${screen} alt="empty list"><p>Your collection list is empty.</p>`;
 
-function renderQueueMovies(){
-    const films = JSON.parse(localStorage.getItem('queue'));
-    
-    if (!films){
-        refs.filmsContainer.innerHTML = '';
-    }else {
-        const markup = films.map(movieTemplate);
-        refs.filmsContainer.innerHTML = markup.join('');
-    }
+    const div = `<div class="film__list-empty-container"><p>Your collection list is empty.</p></div>`;
+
+    refs.filmsContainer.innerHTML = div;
+  } else {
+    const markup = films.map(movieTemplate);
+    refs.filmsContainer.innerHTML = markup.join('');
+  }
 }
 
-export {renderWatchedMovies, renderQueueMovies};
+function renderQueueMovies() {
+  const films = JSON.parse(localStorage.getItem('queue'));
 
+  if (!films) {
+    refs.filmsContainer.innerHTML = '';
+  } else {
+    const markup = films.map(movieTemplate);
+    refs.filmsContainer.innerHTML = markup.join('');
+  }
+}
 
+export { renderWatchedMovies, renderQueueMovies };
