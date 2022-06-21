1389e38c9b22162ba2f8db086002ece258f7b46f
VasylArtymovych <art777vasss@gmail.com>
2022-06-21 13:54:22 +0300

    add render currentPage when reboot

--- src/js/my-library/myLibraryBtn.js
+++ src/js/my-library/myLibraryBtn.js
@@ -2,14 +2,15 @@ import { refs } from '../refs';
 import myLibraryHeaderTemplate from './myLibraryHeaderTemplate';
 import { renderWatchedMovies, renderQueueMovies } from './renderMyLibraryMovies';
 import { checkedThem, switchTheme } from '../switcher';
+import renderMoviesAndPagination from '../renderMoviesAndPagination';
 
 
 refs.myLibraryBtn.addEventListener('click', onMyLibraryBtnClick);
 
 function onMyLibraryBtnClick() {
-  refs.header.classList.add('my-library');
   const markup = myLibraryHeaderTemplate();
-  refs.headerContainer.innerHTML = markup;
+  document.querySelector('.js-header-container').innerHTML = markup;
+  refs.header.classList.add('my-library');
   refs.pagination.innerHTML = '';
 
   const themeSwitch = document.querySelector('#theme-switch');
@@ -17,26 +18,42 @@ function onMyLibraryBtnClick() {
 
   renderWatchedMovies();
   checkedThem();
+
   const currentTheme = localStorage.getItem('theme');
   if (currentTheme === 'light') {
     themeSwitch.checked = false;
   } else {
     themeSwitch.checked = true;
   }
+
   document.querySelector('.js-watched-btn').addEventListener('click', onWatchedBtn);
   document.querySelector('.js-queue-btn').addEventListener('click', onQueueBtn);
-}
+  
+  document.querySelector('.js-logo').addEventListener('click', onLogoAndHomeLibrariHandler);
+  document.querySelector('.js-home-page').addEventListener('click', onLogoAndHomeLibrariHandler);
+
+  localStorage.setItem('library', 'watched');
+};
 
 function onWatchedBtn() {
   renderWatchedMovies();
   document.querySelector('.js-queue-btn').classList.remove('isActive');
   document.querySelector('.js-watched-btn').classList.add('isActive');
-}
+};
 
 function onQueueBtn() {
   renderQueueMovies();
   document.querySelector('.js-watched-btn').classList.remove('isActive');
   document.querySelector('.js-queue-btn').classList.add('isActive');
-}
+};
+
+function onLogoAndHomeLibrariHandler(event){
+  // event.preventDefault()
+  localStorage.setItem('page', '1');
+  localStorage.setItem('library', false);
+  userFilms.setPage(1);
+  renderMoviesAndPagination();
+};
+
 
 export default onMyLibraryBtnClick;
--- src/js/renderMoviesAndPagination.js
+++ src/js/renderMoviesAndPagination.js
@@ -2,16 +2,20 @@ import Notiflix from 'notiflix';
 import { refs } from './refs';
 import { userFilms } from './api';
 import { validationSearchedArray } from './onSearchByKeyWord';
+import movieTemplate from './movieTamplate';
 import createPagination from './pagination';
-import {renderTrendsOnMain} from './renderTrendsOnMain';
 import toggleDragonSpiner from './spiner';
+import onMyLibraryBtnClick from './my-library/myLibraryBtn';
+import {renderQueueMovies} from './my-library/renderMyLibraryMovies';
+
+refs.headerLogo.addEventListener('click', onLogoAndHomeClickHandler);
+refs.headerHomePage.addEventListener('click', onLogoAndHomeClickHandler);
 
 export default function renderMoviesAndPagination() {
   refs.filmsContainer.innerHTML = '';
   if (userFilms.userSearch) {
     toggleDragonSpiner();
-    userFilms
-      .onSearchFilm()
+    userFilms.onSearchFilm()
       .then(({ results, page, total_pages }) => {
         setTimeout(() => {
           if (validationSearchedArray(results)) return;
@@ -23,8 +27,7 @@ export default function renderMoviesAndPagination() {
       .catch(error => Notiflix.Notify.failure('Error!'));
   } else {
     toggleDragonSpiner();
-    userFilms
-      .getTrendingFilm()
+    userFilms.getTrendingFilm()
       .then(({ results, page, total_pages }) => {
         setTimeout(() => {
           renderTrendsOnMain(results);
@@ -34,4 +37,39 @@ export default function renderMoviesAndPagination() {
       })
       .catch(error => Notiflix.Notify.failure('Error!'));
   }
-}
+};
+
+// renderMoviesAndPagination();
+checkWhatPageRender();
+
+function renderTrendsOnMain(films) {
+  const murkup = films.map(movieTemplate);
+  refs.filmsContainer.innerHTML = murkup.join('');
+};
+
+
+export function onLogoAndHomeClickHandler(event){
+  event.preventDefault()
+  localStorage.setItem('page', '1');
+  localStorage.setItem('library', false);
+  userFilms.setPage(1);
+  renderMoviesAndPagination();
+};
+
+
+function checkWhatPageRender(){
+  const library = localStorage.getItem('library');
+  
+  if(library === 'watched'){
+    console.log('render watched page');
+    onMyLibraryBtnClick();
+  }else if (library === 'queue'){
+    console.log('render queue page');
+    onMyLibraryBtnClick();
+    renderQueueMovies();
+    document.querySelector('.js-watched-btn').classList.remove('isActive');
+    document.querySelector('.js-queue-btn').classList.add('isActive');
+  }else {
+    renderMoviesAndPagination();
+  };
+};
